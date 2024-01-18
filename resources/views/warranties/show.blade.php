8c70bf657546bf4f8861699cea98e30b5893c190
Chevy Lin <chevylin0802@gmail.com>
2024-01-18 14:23:22 +0800

    update

--- resources/views/warranties/show.blade.php
+++ resources/views/warranties/show.blade.php
@@ -1,82 +1,56 @@
 @extends('adminlte::page')
 
-@section('title', __('warranties.title'))
+@section('title', __('register.title'))
 
 @section('content_header')
+    <h1 class="m-0 text-dark">{{ __('register.header') }}</h1>
 @stop
-
 <style>
     div.upgrade {
         margin-bottom : 20px;
-                }
-    table {
-        width : 100%;
-        margin-left : 13px;
-        margin-right: 10px;
-        background-color: lightgray;
-          }
-    td {
-        padding-left: 8px;
-        }
+    }
 </style>
 @section('content')
     <div class="row">
-      <img src="/head.jpg" width="100%">
-    </div>
-    @if (false)
-    <div class="row">
       <table>
          <tr>
-            <td><x-adminlte-card title="{{ __('warranties.name') }}" theme="info" icon="fas fa-lg">
-              {{ $warranty->order() ? $warranty->order()->name : null }}
+            <td><x-adminlte-card title="{{ __('register.name') }}" theme="info" icon="fas fa-lg">
+              {{ $warranty->order()->name ?? '' }}
             </x-adminlte-card></td>
-            <td><x-adminlte-card title="{{ __('warranties.phone') }}" theme="info" icon="fas fa-lg">
-              {{ $warranty->order() ? $warranty->order()->phone : null }}
-            </x-adminlte-card></td>
+            <td><x-adminlte-card title="{{ __('register.phone') }}" theme="info" icon="fas fa-lg">
+              {{ $warranty->order()->phone ?? '' }}
+             </x-adminlte-card></td>
          </tr>
          <tr>
-            <td colspan="2"><x-adminlte-card title="{{ __('warranties.address') }}" theme="info" icon="fas fa-lg">
-              {{ $warranty->order() ? $warranty->order()->address : null }}
-            </x-adminlte-card></td>
+            <td colspan="2"><x-adminlte-card title="{{ __('register.address') }}" theme="info" icon="fas fa-lg">
+              {{ $warranty->order()->address ?? '' }}
+             </x-adminlte-card></td>
          </tr>
          <tr>
-            <td><x-adminlte-card title="{{ __('warranties.model_id') }}" theme="info" icon="fas fa-lg">
-              {{ $warranty->productModel() ? $warranty->productModel()->model : null }}
+            <td><x-adminlte-card title="{{ __('register.model_id') }}" theme="info" icon="fas fa-lg">
+              {{ $warranty->productModel()->model ?? '' }}
             </x-adminlte-card></td>
-            <td><x-adminlte-card title="{{ __('warranties.register_time') }}" theme="info" icon="fas fa-lg">
+            <td><x-adminlte-card title="{{ __('register.android_id') }}" theme="info" icon="fas fa-lg">
+              {{ $warranty->product->android_id ?? '' }}
+             </x-adminlte-card></td>
+         </tr>
+         <tr>
+            <td><x-adminlte-card title="{{ __('register.register_time') }}" theme="info" icon="fas fa-lg">
               {{ $warranty->register_time }}
             </x-adminlte-card></td>
+            <td><x-adminlte-card title="{{ __('register.warranty_date') }}" theme="info" icon="fas fa-lg">
+              {{ date('Y-m-d', strtotime('+3 years', strtotime($warranty->register_time))) }}
+            </x-adminlte-card></td>
          </tr>
-      </table>
-    </div>
-    @endif
-    <div class="row">
-      <table border="1">
-      <tr>
-         <td>{{ __('warranties.name') }}</td>
-         <td>{{ $warranty->order() ? $warranty->order()->name : null }}</td>
-         <td>{{ __('warranties.phone') }}</td>
-         <td>{{ $warranty->order() ? $warranty->order()->phone : null }}</td>
-      </tr>
-      <tr>
-         <td>{{ __('warranties.address') }}</td>
-         <td colspan="3">{{ $warranty->order() ? $warranty->order()->address : null }}</td>
-      </tr>
-      <tr>
-         <td>{{ __('warranties.model_id') }}</td>
-         <td colspan="3">{{ $warranty->productModel() ? $warranty->productModel()->model : null }}</td>
-      </tr>
-      <tr>
-         <td>{{ __('warranties.serialno') }}</td>
-         <td colspan="3">{{ null }}</td>
-      </tr>
-      <tr>
-         <td>{{ __('warranties.register_time') }}</td>
-         <td colspan="3">{{ $warranty->register_time }}</td>
-      </tr>
-      <tr>
-         <td colspan="4"></td>
-      </tr>
+         <tr>
+            <td colspan="2"><h2>{{ __('register.company') }}</h3></td>
+         </td>
+         <tr>
+            <td colspan="2"><h3>{{ __('register.caddress') }}</h3></td>
+         </td>
+         <tr>
+            <td colspan="2"><h3>{{ __('register.cphone') }}</h3></td>
+         </td>
       </table>
     </div>
 @endsection
