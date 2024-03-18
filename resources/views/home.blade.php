2b6de314bab6bb79d93cffb578245975026b22cb
Chevy Lin <chevylin0802@gmail.com>
2024-03-18 10:36:43 +0800

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,9 @@
 @section('content')
     <div class="row col-12">
       <x-adminlte-card title="改版公告" theme="dark" icon="fas fa-lg fa-fan">
+        小改版 : 2024年3月10日 (名稱異動)
+      </x-adminlte-card>
+      <x-adminlte-card title="改版公告" theme="dark" icon="fas fa-lg fa-fan">
         小改版 : 2023年4月24日 (表格支持排序功能以及搜尋)
       </x-adminlte-card>
     </div>
